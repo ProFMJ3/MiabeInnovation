0f94e02c959169bb2001699bd54fcd34f2dbe1ee
ProFMJ3 <follykoejules@gmail.com>
2025-05-01 12:53:55 +0000

    mise à jour de l'environnement

--- MI/settings.py
+++ MI/settings.py
@@ -63,6 +63,7 @@ WSGI_APPLICATION = 'MI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
