a184f4b0c0e2adb34b323298c701548978c35c5b
ProFMJ3 <follykoejules@gmail.com>
2025-05-04 19:01:14 +0000

    Mise à jour

--- SiteVitrine/views.py
+++ SiteVitrine/views.py
@@ -3,5 +3,41 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def home(request):
-
     return render(request, 'index.html')
+
+
+def quiSommeNous(request):
+    return render(request, 'pages/quiSommeNous.html')
+
+
+def equipe(request):
+    return render(request, 'pages/equipe.html')
+
+
+def service(request):
+    return render(request, 'pages/service.html')
+
+
+def temoignages(request):
+    return render(request, 'pages/temoignages.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+
+
+
+def faqs(request):
+    return render(request, 'pages/faqs.html')
+
+
+
+
+
+
+
+
+
+
+
