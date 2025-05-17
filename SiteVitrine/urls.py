b6a643f2bba1802dab8c672048994bf330b3997d
ProFMJ3 <follykoejules@gmail.com>
2025-05-17 00:55:19 +0000

    Backend

--- SiteVitrine/urls.py
+++ SiteVitrine/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('quiSommesNous/', views.quiSommesNous, name='qui_sommes_nous'),
     path('equipe/', views.equipe, name='equipe'),
     path('blog/', views.blog, name='blog'),
-   
+
     path('temoignages/', views.temoignages, name='temoignages'),
     path('entreeContact/', views.entreeContact, name='entree_contact'),
     path('devenirPartenaire/', views.devenirPartenaire, name='devenir_partenaire'),
